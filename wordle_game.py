4786a84e74567c005e3a0939597033b3417b1356
Arda Yılmazlar <ardayilmazlar@yandex.com>
2024-09-01 21:01:59 +0300

    Fixed the order of displayed letters on UI

--- wordle_game.py
+++ wordle_game.py
@@ -1,5 +1,5 @@
 from random import randint
-from typing import List, Dict
+from typing import List, Tuple
 from enums import GuessStatus
 
 class WordleGame:
@@ -18,21 +18,21 @@ class WordleGame:
     def _select_random_word(self) -> str:
         return self.word_list[randint(0, len(self.word_list))]
     
-    def make_attempt(self, attempted_word: str) -> Dict[str, int]:
+    def make_attempt(self, attempted_word: str) -> List[Tuple[str, int]]:
         if attempted_word not in self.word_list:
             raise ValueError(f"'{attempted_word}' is not a valid word.")
         elif attempted_word in self.attempts:
             raise ValueError(f"'{attempted_word}' have been guessed before.")
         
-        result: Dict[str, int] = dict()
+        result: List[Tuple[str, int]] = list()
         
         for i, char in enumerate(attempted_word):
             if char == self.target_word[i]:
-                result[char] = GuessStatus.CORRECT.value
+                result.append((char, GuessStatus.CORRECT.value))
             elif char in self.target_word:
-                result[char] = GuessStatus.PRESENT.value
+                result.append((char, GuessStatus.PRESENT.value))
             else:
-                result[char] = GuessStatus.ABSENT.value
+                result.append((char, GuessStatus.ABSENT.value))
             
         self.attempts.append(attempted_word)
         return result
