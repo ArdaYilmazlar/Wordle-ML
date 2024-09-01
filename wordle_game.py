ad26efc33927a20d621b415e14f3dab56d6c8f32
Arda Yılmazlar <ardayilmazlar@yandex.com>
2024-09-01 20:14:52 +0300

    game_over check

--- wordle_game.py
+++ wordle_game.py
@@ -7,11 +7,13 @@ class WordleGame:
     word_list: List[str]
     attempts: List[str]
     target_word: str
+    max_attempts: int
 
-    def __init__(self, word_list: List[str]):
+    def __init__(self, word_list: List[str], max_attempts: int = 6):
         self.word_list = word_list
         self.target_word = self._select_random_word()
-        self.target_word = list()
+        self.attempts = list()
+        self.max_attempts = max_attempts
     
     def _select_random_word(self) -> str:
         return self.word_list[randint(0, len(self.word_list))]
@@ -34,4 +36,7 @@ class WordleGame:
             
         self.attempts.append(attempted_word)
         return result
+    
+    def is_game_over(self) -> bool:
+        return len(self.attempts) >= self.max_attempts or self.attempts[-1] == self.target_word
 
